4702ed0e095b476e980d158a82616168352c949d
Evan Burriola <evanburriola12@tamu.edu>
2023-05-19 15:57:38 -0500

    Added totem drop on death assigned to player. Added resurrection by alter. Only 1 resurrection per player.

--- src/main/java/risobewee_hardcore/events/ModServerEvents.java
+++ src/main/java/risobewee_hardcore/events/ModServerEvents.java
@@ -1,5 +1,10 @@
 package risobewee_hardcore.events;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import risobewee_hardcore.RisobEwee_HardcoreMain;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -10,20 +15,69 @@ import net.minecraft.world.level.GameType;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import risobewee_hardcore.item.ModItems;
 
 
 @Mod.EventBusSubscriber(modid = RisobEwee_HardcoreMain.MOD_ID)
 public class ModServerEvents {
 
+    @SubscribeEvent
+    public static void dropTotemOnDeath(LivingDeathEvent event){
+        if(event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            ItemStack totemStack = new ItemStack(ModItems.TOTEM_OF_RESURRECTION.get());
+            //Put player 'soul' -> display name on item stack
+            CompoundTag nbtData = new CompoundTag();
+            nbtData.putString("risobewee_hardcore.soul", player.getDisplayName().getString());
+            totemStack.setTag(nbtData);
+
+            //Drop totem at death position
+            double x = player.getX();
+            double y = player.getY();
+            double z = player.getZ();
+            player.getLevel().addFreshEntity(new ItemEntity(player.getLevel(), x, y, z, totemStack));
+            RisobEwee_HardcoreMain.LOGGER.info("Dropped totem");
+        }
+    }
+
+    //Event handler: First death makes player respawn in spectator. Second "death" (Next time "Respawn" is clicked)
+    // player is set to survival with only 5 hearts.
+    @SubscribeEvent
+    public static void onRespawnEvent(PlayerEvent.PlayerRespawnEvent event){
+        Player player = event.getPlayer();
+        if(!player.getTags().contains("full_dead"))
+            player.addTag("resurrectable");
+        setPlayerGameMode(player, GameType.SPECTATOR);
+        RisobEwee_HardcoreMain.LOGGER.info("onRespawnEvent Triggered");
+    }
+
+
+
+    //Adds tags to indicate the upcoming death(Player revived after first death will have tag death2)
+    public static void resurrectFromAlter(Player player, BlockPos alterBlock){
+        if(player.getTags().contains("resurrectable")) {
+            //To teleport revived player above alter
+            double x = alterBlock.above().getX();
+            double y = alterBlock.above().getY();
+            double z = alterBlock.above().getZ();
+            player.removeTag("resurrectable");
+            player.teleportTo(x, y, z);
+            setPlayerGameMode(player, GameType.SURVIVAL);
+            reducePlayerHealth(player, 14);
+            player.addTag("full_dead");
+        }
+        RisobEwee_HardcoreMain.LOGGER.info("Resurrect from alter Triggered");
+    }
+
     //Need to test for server sided events. May have to use ServerPlayer & Server event handlers..?
-    private static void reducePlayerHealth(Player player){
+    private static void reducePlayerHealth(Player player, int newHealth){
         Component displayName = player.getDisplayName();
         CommandSourceStack cst = player.createCommandSourceStack().withMaximumPermission(2);
         Commands c = new Commands(Commands.CommandSelection.ALL);
 
         player.setInvulnerable(false);
         c.performCommand(cst, "attribute " + displayName.getString() + " minecraft:generic.max_health base set 10");
-        player.hurt(DamageSource.FALL, 10);
+        player.hurt(DamageSource.FALL, newHealth);
 
         RisobEwee_HardcoreMain.LOGGER.info(displayName.getString() + " had their health reduced!");
     }
@@ -39,22 +93,5 @@ public class ModServerEvents {
             c.performCommand(cst, "gamemode survival " + displayName.getString());
         }
     }
-
-    //Event handler: First death makes player respawn in spectator. Second "death" (Next time "Respawn" is clicked)
-    // player is set to survival with only 5 hearts.
-    @SubscribeEvent
-    public static void onRespawnEvent(PlayerEvent.PlayerRespawnEvent event){
-        Player player = event.getPlayer();
-        if(player.getTags().contains("has_died")) {
-            player.removeTag("has_died");
-            setPlayerGameMode(player,GameType.SURVIVAL);
-            reducePlayerHealth(player);
-        } else{
-            player.addTag("has_died");
-            setPlayerGameMode(player,GameType.SPECTATOR);
-        }
-        RisobEwee_HardcoreMain.LOGGER.info("onRespawnEvent Triggered");
-    }
-
 
 }
--- src/main/java/risobewee_hardcore/item/custom/TotemOfResurrection.java
+++ src/main/java/risobewee_hardcore/item/custom/TotemOfResurrection.java
@@ -1,26 +1,73 @@
 package risobewee_hardcore.item.custom;
 
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntitySelector;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import risobewee_hardcore.RisobEwee_HardcoreMain;
+import risobewee_hardcore.events.ModServerEvents;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
 
 public class TotemOfResurrection extends Item {
+
     public TotemOfResurrection(Properties pProperties) {
         super(pProperties);
+    }
+
+
+    //Display player 'soul' => display name on item description
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced){
+        if(pStack.hasTag()){
+            String playerSoul = pStack.getTag().getString("risobewee_hardcore.soul");
+            pTooltipComponents.add(new TextComponent(playerSoul));
+        }
+        super.appendHoverText(pStack,pLevel,pTooltipComponents,pIsAdvanced);
     }
 
     @Override
     public InteractionResult useOn(UseOnContext pContext){
         if(pContext.getLevel().isClientSide){
+            RisobEwee_HardcoreMain.LOGGER.info("CLIENTSIDE");
+        } else {
             BlockPos posClicked = pContext.getClickedPos();
             Block block = pContext.getLevel().getBlockState(posClicked).getBlock();
             if(block.getRegistryName().toString().equals("risobewee_hardcore:resurrection_block")){
                 RisobEwee_HardcoreMain.LOGGER.info("BLOCK CLICKED");
+                List<Player> spectators = (List<Player>) pContext.getLevel().players().stream().filter(EntitySelector.NO_SPECTATORS.negate()).toList();
+
+                Player playerSoul = null;
+                RisobEwee_HardcoreMain.LOGGER.info("spectators");
+                for(Player p : spectators){
+                    RisobEwee_HardcoreMain.LOGGER.info(p.getDisplayName().getString());
+                    RisobEwee_HardcoreMain.LOGGER.info(pContext.getItemInHand().getTag().getString("risobewee_hardcore.soul"));
+                    if(pContext.getItemInHand().hasTag()){
+                        if(p.getDisplayName().getString().equals(pContext.getItemInHand().getTag().getString("risobewee_hardcore.soul"))){
+                            playerSoul = p;
+                            break;
+                        }
+                    }
+                }
+
+                if(playerSoul != null) {
+                    ModServerEvents.resurrectFromAlter(playerSoul, posClicked);
+                }
+
             }
         }
         return super.useOn(pContext);
     }
+
 }
